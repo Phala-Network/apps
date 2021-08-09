d29a323f38bfedc144de4168ad1537770084e7f0
limichange <limichange@hotmail.com>
2021-08-09 16:17:22 +0800

    feat: config gatsby

---
+++ packages/app/gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: `Phala App`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@phala`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+  },
+  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-typescript`],
+}
