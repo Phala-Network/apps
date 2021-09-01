ebb4dd43a6eb6ddbf0c2ed7563efb1317f8311c8
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2021-09-01 21:42:46 +0800

    test: set network to para1

--- packages/app/src/WrapApp.tsx
+++ packages/app/src/WrapApp.tsx
@@ -23,7 +23,7 @@ const WrapApp: React.FC = ({children}) => {
   console.info('defaultNetwork', defaultNetwork)
 
   const productionConfig = {
-    defaultNetwork,
+    defaultNetwork: 'para1',
     substrateGraphEndpoint: 'https://subquery-api.phala.network',
     ethereumGraphEndpoint:
       'https://graphs-api.phala.network/subgraphs/name/chainbridge',
