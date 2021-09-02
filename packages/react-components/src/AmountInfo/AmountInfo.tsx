a8dc45c635824dea8858d03ce246300e0bbb1e3c
limichange <limichange@hotmail.com>
2021-09-02 16:27:16 +0800

    refactor(react-components): AmountInfo

--- packages/react-components/src/AmountInfo/AmountInfo.tsx
+++ packages/react-components/src/AmountInfo/AmountInfo.tsx
@@ -1,44 +1,7 @@
 import {toFixed} from '@phala/utils'
 import React, {ComponentProps} from 'react'
-import styled from 'styled-components'
-import Tag from './Tag'
-
-const AmountInfoWrap = styled.div`
-  background: #ececec;
-  padding: 16px;
-`
-
-const Amount = styled.div`
-  color: #494949;
-  font-size: 20px;
-  margin-right: 8px;
-`
-
-const Header = styled.div`
-  font-family: PT Mono;
-  display: flex;
-  align-items: center;
-`
-
-const Type = styled.div`
-  flex: 1;
-  font-family: Lato;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 19px;
-  display: flex;
-  align-items: center;
-  color: #494949;
-  text-transform: capitalize;
-`
-
-const Divider = styled.div`
-  opacity: 0.2;
-  background-color: #000000;
-  height: 1px;
-  margin: 16px 0;
-`
+import Tag from '../Tag'
+import {Amount, AmountInfoWrap, Divider, Header, Type} from './styledComponents'
 
 type Props = {
   amount?: number | string
