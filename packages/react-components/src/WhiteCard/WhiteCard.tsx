b931e96bc40fa30742c883942d11084b86bbf2c2
limichange <limichange@hotmail.com>
2021-09-03 15:04:10 +0800

    refactor(react-components): WhiteCard

--- packages/react-components/src/WhiteCard/WhiteCard.tsx
+++ packages/react-components/src/WhiteCard/WhiteCard.tsx
@@ -1,10 +1,8 @@
 import styled from 'styled-components'
 
-const WhiteCard = styled.div`
+export const WhiteCard = styled.div`
   width: 100%;
   padding: 20px;
   background-color: white;
   box-sizing: border-box;
 `
-
-export default WhiteCard
