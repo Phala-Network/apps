382aaefe9b5c6788b9ed43e7eccdef9e144c59a3
limichange <limichange@hotmail.com>
2021-09-03 17:00:11 +0800

    refactor(react-components): InputNumber

--- packages/react-components/src/Input/index.ts
+++ packages/react-components/src/Input/index.ts
@@ -1,1 +1,2 @@
 export {Input} from './Input'
+export type {InputProps} from './Input'
--- packages/react-components/src/InputNumber/InputNumber.tsx
+++ packages/react-components/src/InputNumber/InputNumber.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import Input, {InputProps} from './Input'
 import {InputNumberProps} from 'rc-input-number'
+import React from 'react'
+import {Input, InputProps} from '../Input'
 
 type Props = InputProps & Omit<InputNumberProps<number>, 'size'>
 
-const InputNumber: React.FC<Props> = (props) => {
+export const InputNumber: React.FC<Props> = (props) => {
   const {size, ...others} = props
 
   return <Input size={size} {...others} type="number"></Input>
 }
-
-export default InputNumber
