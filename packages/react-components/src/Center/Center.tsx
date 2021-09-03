981aa6c9b595dcdf73fc261fb2ce957a20d1b917
limichange <limichange@hotmail.com>
2021-09-03 15:00:54 +0800

    refactor(react-components): Center

--- packages/react-components/src/Center/Center.tsx
+++ packages/react-components/src/Center/Center.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-const Center = styled.div`
+export const Center = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -8,5 +8,3 @@ const Center = styled.div`
   width: 100%;
   height: 100%;
 `
-
-export default Center
