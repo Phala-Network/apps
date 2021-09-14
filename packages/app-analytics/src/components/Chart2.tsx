d1267f7eb2d85b131286637ad6074ec591e504a0
limichange <limichange@hotmail.com>
2021-09-14 10:57:45 +0800

    feat(app-analytics): update Chart2 echart config

--- packages/app-analytics/src/components/Chart2.tsx
+++ packages/app-analytics/src/components/Chart2.tsx
@@ -1,111 +1,113 @@
 import ReactECharts from 'echarts-for-react'
-import React, {useEffect, useMemo} from 'react'
+import React, {useEffect} from 'react'
+
+const defaultChartOptions = {
+  tooltip: {
+    trigger: 'axis',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    borderColor: 'rgba(255, 255, 255, 0.4)',
+    textStyle: {
+      color: 'white',
+    },
+  },
+  xAxis: {
+    type: 'time',
+    splitLine: {
+      show: true,
+      lineStyle: {
+        opacity: 0.1,
+        type: 'dashed',
+      },
+    },
+    splitNumber: 20,
+    // axisLabel: {
+    //   formatter(value) {
+    //     const date = new Date(value)
+
+    //     return [date.getMonth() + 1, date.getDate()].join('.')
+    //   },
+    // },
+  },
+  grid: [
+    {
+      left: '0px',
+      right: '0px',
+      bottom: '20px',
+      top: '20px',
+    },
+  ],
+  yAxis: [
+    {
+      type: 'value',
+      name: 'Amount',
+      splitLine: {show: false},
+      axisPointer: {show: false},
+      show: false,
+    },
+    {
+      type: 'value',
+      name: 'PHA',
+      splitLine: {show: false},
+      axisPointer: {show: false},
+      show: false,
+    },
+  ],
+  series: [
+    {
+      name: 'KSM',
+      type: 'line',
+      itemStyle: {color: '#eb5757'},
+      showSymbol: false,
+      yAxisIndex: 0,
+      data: [],
+    },
+    {
+      name: 'PHA',
+      type: 'line',
+      itemStyle: {color: '#03FFFF'},
+      showSymbol: false,
+      yAxisIndex: 1,
+      data: [],
+    },
+  ],
+}
 
 export const Chart2: React.FC = () => {
-  const [campaignData, setCampaignData] = React.useState<any>({})
+  const [PHA, setPHA] = React.useState<any>({})
+  const [KSM, setKSM] = React.useState<any>({})
 
   useEffect(() => {
-    fetch('https://crowdloan-api.phala.network/campaigns/1')
+    fetch('https://crowdloan-api.phala.network/coin_market_charts/PHA')
       .then((response) => response.json())
       .then((res) => {
-        setCampaignData(res)
+        setPHA(res)
       })
   }, [])
 
-  const contributionChart = useMemo(
-    () => campaignData?.meta?.contribution_chart || [],
-    [campaignData?.meta?.contribution_chart]
-  )
+  useEffect(() => {
+    fetch('https://crowdloan-api.phala.network/coin_market_charts/KSM')
+      .then((response) => response.json())
+      .then((res) => {
+        setKSM(res)
+      })
+  }, [])
 
   const chartOptions = React.useMemo(() => {
-    if (!contributionChart || contributionChart.length === 0) {
-      return {}
-    }
+    const formatData = (item: [string, number]) => [item[0], item[1].toFixed(2)]
 
-    let [, maxValue] = contributionChart[contributionChart.length - 1]
-    maxValue = maxValue > 30000 ? maxValue : 30000
-
-    return {
-      tooltip: {
-        trigger: 'axis',
-        backgroundColor: 'rgba(0, 0, 0, 0.6)',
-        borderColor: 'rgba(255, 255, 255, 0.4)',
-        textStyle: {
-          color: 'white',
-        },
-      },
-      grid: [
-        {
-          top: '5px',
-          right: '45px',
-          left: '0px',
-          bottom: '24px',
-        },
-      ],
-      xAxis: {
-        type: 'time',
-        splitLine: {
-          show: true,
-          lineStyle: {
-            opacity: 0.1,
-            type: 'dashed',
-          },
-        },
-      },
-      yAxis: [
-        {
-          show: true,
-          splitLine: {
-            show: true,
-            lineStyle: {
-              opacity: 0.1,
-              type: 'dashed',
-            },
-          },
-          axisLabel: {
-            formatter(value: number) {
-              if (value === 30000) {
-                return '\n30,000\n1:150'
-              } else if (value === 0) {
-                return '1:120'
-              } else {
-                return ''
-              }
-            },
-          },
-          max: maxValue,
-          position: 'right',
-          name: 'PHA',
-          interval: 30000,
-          type: 'value',
-        },
-      ],
+    return Object.assign({}, defaultChartOptions, {
       series: [
         {
-          yAxisIndex: 0,
-          name: 'PHA',
-          type: 'line',
-          itemStyle: {color: '#03FFFF'},
-          showSymbol: false,
-          data: contributionChart,
-          markLine: {
-            silent: true,
-            label: {
-              color: 'rgba(255, 255, 255, 0.4)',
-              borderWidth: 0,
-              formatter: () => '',
-            },
-            symbol: ['none', 'none'],
-            data: [
-              {
-                yAxis: 30000,
-              },
-            ],
-          },
+          ...defaultChartOptions.series[0],
+          data: KSM?.data?.map?.(formatData),
+        },
+        {
+          ...defaultChartOptions.series[1],
+          data: PHA?.data?.map?.(formatData),
         },
       ],
-    }
-  }, [contributionChart])
+    })
+  }, [PHA, KSM])
 
   return (
     <ReactECharts
