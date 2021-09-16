053233f646b4ee9618e78c0445082a2edc9b4665
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2021-09-16 15:16:55 +0800

    chore: page title template

--- packages/app-assets/src/index.tsx
+++ packages/app-assets/src/index.tsx
@@ -74,11 +74,7 @@ const HomePage: React.FC = () => {
   return (
     <>
       <Helmet>
-        <title>Phala App</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
+        <title>Assets</title>
       </Helmet>
       <ContentWrapper>
         <Category
--- packages/app/src/WrapPage.tsx
+++ packages/app/src/WrapPage.tsx
@@ -1,3 +1,4 @@
+import {Helmet} from 'react-helmet'
 import {ComponentsGlobalStyle} from '@phala/react-components'
 import {Slide, ToastContainer} from 'react-toastify'
 import BaseLayout from './components/BaseLayout'
@@ -5,6 +6,12 @@ import GlobalStyle from './GlobalStyle'
 
 const WrapPage: React.FC = ({children}) => (
   <>
+    <Helmet titleTemplate="%s | Phala App">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+      />
+    </Helmet>
     <GlobalStyle />
     <ComponentsGlobalStyle />
     <BaseLayout>{children}</BaseLayout>
