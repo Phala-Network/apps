3800f0964c2c066d8e5b41859ed4f625a221ae0e
limichange <limichange@hotmail.com>
2021-09-29 19:23:03 +0800

    feat: analytics chart

--- packages/app-analytics/src/AppAnalytics.tsx
+++ packages/app-analytics/src/AppAnalytics.tsx
@@ -1,3 +1,4 @@
+import {useCallback, useEffect, useState} from 'react'
 import {down} from 'styled-breakpoints'
 import styled, {createGlobalStyle} from 'styled-components'
 import {BlackCard, Chart, Chart2, Info} from './components'
@@ -28,22 +29,46 @@ const HideBMapIcon = createGlobalStyle`
   }
 `
 
-export const AppAnalytics = () => (
-  <>
-    <HideBMapIcon></HideBMapIcon>
-    <Root>
-      <Info></Info>
+export type AnalyticsData = {
+  date: string
+  onlineWorkers: number
+  reward: number
+  vCPU: number
+  workers: number
+}[]
 
-      <h2>Chart</h2>
+export const AppAnalytics = () => {
+  const [analyticsData, setAnalyticsData] = useState<AnalyticsData>()
+  const getData = useCallback(async function getData() {
+    const response = await fetch(
+      'https://app-analytics-data.netlify.app/chart/chartData.json'
+    )
+    const data = await response.json()
 
-      <Charts>
-        <BlackCard>
-          <Chart></Chart>
-        </BlackCard>
-        <BlackCard>
-          <Chart2></Chart2>
-        </BlackCard>
-      </Charts>
-    </Root>
-  </>
-)
+    setAnalyticsData(data)
+  }, [])
+
+  useEffect(() => {
+    getData()
+  }, [getData])
+
+  return (
+    <>
+      <HideBMapIcon></HideBMapIcon>
+      <Root>
+        <Info></Info>
+
+        <h2>Chart</h2>
+
+        <Charts>
+          <BlackCard>
+            <Chart2 data={analyticsData}></Chart2>
+          </BlackCard>
+          <BlackCard>
+            <Chart data={analyticsData}></Chart>
+          </BlackCard>
+        </Charts>
+      </Root>
+    </>
+  )
+}
