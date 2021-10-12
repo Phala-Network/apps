d9e87a39ff86e34032b22431ad19c6b5fe3e3c15
limichange <limichange@hotmail.com>
2021-10-12 16:19:37 +0800

    feat: display transaction etherscan address

--- packages/react-components/src/bridge/KhalaProcess.tsx
+++ packages/react-components/src/bridge/KhalaProcess.tsx
@@ -3,23 +3,36 @@ import Progress from './Progress'
 
 interface ProgressProps {
   progressIndex: number
+  khalaAddress: string
+  etherscanAddress: string
 }
 
 export const KhalaProcess: FC<ProgressProps> = (props) => {
-  const {progressIndex} = props
+  const {progressIndex, etherscanAddress} = props
 
+  // https://kovan.etherscan.io/address/0xdf2e83f33db8a9ccf3a00fce18c3f509b974353d#tokentxns
   const steps = [
     {
       text: 'Transaction Send',
     },
     {
       text: 'Khala Confirmed',
+      // ...(progressIndex > 0
+      //   ? {
+      //       link: `https://kovan.etherscan.io/address/${khalaAddress}#tokentxns`,
+      //     }
+      //   : {}),
     },
     {
       text: 'Relayer Confirmed',
     },
     {
       text: 'Ethereum Confirmed',
+      ...(progressIndex === 3
+        ? {
+            link: `https://kovan.etherscan.io/address/${etherscanAddress}#tokentxns`,
+          }
+        : {}),
     },
   ]
 
