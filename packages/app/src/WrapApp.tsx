4fc50a259ebcd67307f3e77518ccf66c0faa00f1
limichange <limichange@hotmail.com>
2021-11-02 17:29:49 +0800

    feat: create useEthFee

--- packages/app/src/WrapApp.tsx
+++ packages/app/src/WrapApp.tsx
@@ -3,7 +3,6 @@ import {getCMSLog} from '@phala/react-cms'
 import {MobileToastContextProvider} from '@phala/react-components'
 import {I18nextProvider} from '@phala/react-i18n'
 import {Provider as LibProvider} from '@phala/react-libs'
-import {isDev, isTest} from '@phala/utils'
 import * as Sentry from '@sentry/react'
 import React, {useLayoutEffect, useRef} from 'react'
 import {Helmet} from 'react-helmet'
@@ -27,9 +26,9 @@ const WrapApp: React.FC = ({children}) => {
     })
   )
 
-  const defaultNetwork =
-    process.env['GATSBY_DEFAULT_NETWORK'] ||
-    (isDev() || isTest() ? 'khala-pc-test' : 'khala')
+  const defaultNetwork = 'khala'
+  // process.env['GATSBY_DEFAULT_NETWORK'] ||
+  // (isDev() || isTest() ? 'khala-pc-test' : 'khala')
 
   // eslint-disable-next-line no-console
   console.info('defaultNetwork', defaultNetwork)
