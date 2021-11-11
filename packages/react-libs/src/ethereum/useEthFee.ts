a4f436ea816e6a8b8d7e038b93f461a84d303f21
limichange <limichange@hotmail.com>
2021-11-11 10:57:35 +0800

    feat: temp save

--- packages/react-libs/src/ethereum/useEthFee.ts
+++ packages/react-libs/src/ethereum/useEthFee.ts
@@ -1,33 +1,87 @@
-import {ethers} from 'ethers'
-import {useState} from 'react'
-import {useBridgeContract} from '..'
+import {BigNumber, ethers} from 'ethers'
+import {isHexString} from 'ethers/lib/utils'
+import {useMemo} from 'react'
+import {useNetworkContext} from '../polkadot/hooks/useSubstrateNetwork'
+import {useBridgeContract} from './bridge/useBridgeContract'
+import {useEthers} from './contexts/useEthers'
 import {useEthereumNetworkOptions} from './queries/useEthereumNetworkOptions'
+import {useEthersNetworkQuery} from './queries/useEthersNetworkQuery'
+// import {useNetworkContext} from '../../polkadot/hooks/useSubstrateNetwork'
+// import {useEthers} from '../contexts/useEthers'
+// import {useEthereumNetworkOptions} from '../queries/useEthereumNetworkOptions'
+// import {useEthersNetworkQuery} from '../queries/useEthersNetworkQuery'
+// import {useBridgeContract} from './useBridgeContract'
 
-export async function useEthFee() {
-  const amount = ethers.utils.parseUnits('10', 18)
-  const recipient = '0xC84456ecA286194A201F844993C220150Cf22C63'
-  const [ethFee] = useState(0)
-  // const {provider} = useEthers()
-  const bridgeContract = useBridgeContract()
+type DepositSubmitFn = (
+  amount: BigNumber,
+  recipient: string
+) => Promise<ethers.providers.TransactionResponse> // TODO: use HexString
+
+/**
+ * Submits a transfer of ERC-20 tokens from Ethereum to Substrate
+ */
+export const useErc20Deposit = (
+  sender?: string
+): DepositSubmitFn | undefined => {
+  const {contract} = useBridgeContract()
+  const {network: substrateName} = useNetworkContext()
   const {options: config} = useEthereumNetworkOptions()
-  // const {options} = useNetworkContext()
-  const destChainId = 1
+  const {data: network} = useEthersNetworkQuery()
+  const {provider} = useEthers()
 
-  const amountPayload = ethers.utils
-    .hexZeroPad(amount.toHexString(), 32)
-    .substr(2)
-  const recipientPayload = recipient.substr(2)
-  const recipientSize = ethers.utils
-    .hexZeroPad(ethers.utils.hexlify(recipientPayload.length / 2), 32)
-    .substr(2)
+  const bridge = useMemo(() => {
+    return contract !== undefined && provider !== undefined
+      ? contract.connect(provider.getSigner(sender))
+      : undefined
+  }, [contract, provider, sender])
 
-  const payload = `0x${amountPayload}${recipientSize}${recipientPayload}`
+  return useMemo(() => {
+    if (
+      bridge === undefined ||
+      config === undefined ||
+      network === undefined ||
+      sender === undefined ||
+      substrateName === undefined
+    ) {
+      return undefined
+    }
 
-  bridgeContract.contract?.estimateGas
-    ?.deposit?.(destChainId, config?.erc20ResourceId, payload)
-    .then((gasLimit) => {
-      console.error(gasLimit)
-    })
+    const destChainId = config.peerChainIds[substrateName as string] as
+      | number
+      | undefined
 
-  return ethFee
+    return async (amount, recipient) => {
+      if (destChainId === undefined) {
+        throw new Error(
+          `Unsupported Ethereum network: ${network.name} (${network.chainId})`
+        )
+      }
+
+      if (typeof bridge.functions['deposit'] !== 'function') {
+        throw new Error(
+          'Assertion failed: deposit should be available on the bridge contract'
+        )
+      }
+
+      if (!isHexString(recipient)) {
+        throw new Error('Validation failed: recipient should be hex string')
+      }
+
+      const amountPayload = ethers.utils
+        .hexZeroPad(amount.toHexString(), 32)
+        .substr(2)
+      const recipientPayload = recipient.substr(2)
+      const recipientSize = ethers.utils
+        .hexZeroPad(ethers.utils.hexlify(recipientPayload.length / 2), 32)
+        .substr(2)
+
+      const payload = `0x${amountPayload}${recipientSize}${recipientPayload}`
+
+      return await (bridge.functions['deposit'](
+        destChainId,
+        config.erc20ResourceId,
+        payload
+      ) as Promise<ethers.providers.TransactionResponse>)
+    }
+  }, [bridge, config, network, sender, substrateName])
 }
