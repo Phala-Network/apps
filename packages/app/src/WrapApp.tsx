668b612f4245752b88b4592bbb2942b6ffc9dfee
limichange <limichange@hotmail.com>
2021-11-16 10:56:00 +0800

    fix: export name error

--- packages/app/src/WrapApp.tsx
+++ packages/app/src/WrapApp.tsx
@@ -3,6 +3,7 @@ import {getCMSLog} from '@phala/react-cms'
 import {MobileToastContextProvider} from '@phala/react-components'
 import {I18nextProvider} from '@phala/react-i18n'
 import {Provider as LibProvider} from '@phala/react-libs'
+import {isDev, isTest} from '@phala/utils'
 import * as Sentry from '@sentry/react'
 import React, {useLayoutEffect, useRef} from 'react'
 import {Helmet} from 'react-helmet'
@@ -26,9 +27,11 @@ const WrapApp: React.FC = ({children}) => {
     })
   )
 
-  const defaultNetwork = 'khala'
-  // process.env['GATSBY_DEFAULT_NETWORK'] ||
-  // (isDev() || isTest() ? 'khala-pc-test' : 'khala')
+  let defaultNetwork =
+    process.env['GATSBY_DEFAULT_NETWORK'] ||
+    (isDev() || isTest() ? 'khala-pc-test' : 'khala')
+
+  defaultNetwork = 'khala'
 
   // eslint-disable-next-line no-console
   console.info('defaultNetwork', defaultNetwork)
