287dd4bffcdbc3852fa2b6ef35b9d8767aa2030d
limichange <limichange@hotmail.com>
2021-11-26 18:26:33 +0800

    feat: AddWorkerModal add fee label

--- packages/app-mining/src/components/StakePoolTable/AddWorkerModal.tsx
+++ packages/app-mining/src/components/StakePoolTable/AddWorkerModal.tsx
@@ -1,9 +1,11 @@
+import {usePolkadotAccountAtom} from '@phala/app-store'
 import {Input} from '@phala/react-components'
 import {
   useApiPromise,
   useDecimalJsTokenDecimalMultiplier,
+  usePhalaStakePoolTransactionFee,
 } from '@phala/react-libs'
-import {useCallback, useState} from 'react'
+import {useCallback, useMemo, useState} from 'react'
 import type {StakePoolModalProps} from '.'
 import useWaitSignAndSend from '../../hooks/useWaitSignAndSend'
 import ActionModal, {Label, Value} from '../ActionModal'
@@ -14,17 +16,25 @@ const AddWorkerModal = (props: StakePoolModalProps): JSX.Element => {
   const waitSignAndSend = useWaitSignAndSend()
   const decimals = useDecimalJsTokenDecimalMultiplier(api)
   const [pubkey, setPubkey] = useState<string>('')
+  const [polkadotAccount] = usePolkadotAccountAtom()
+
+  const action = useMemo(() => {
+    if (!api || !pubkey || !decimals) return
+
+    return api.tx.phalaStakePool?.addWorker?.(stakePool.pid, pubkey)
+  }, [api, stakePool, pubkey, decimals])
 
   const onConfirm = useCallback(async () => {
-    if (api && decimals && pubkey) {
-      return waitSignAndSend(
-        api.tx.phalaStakePool?.addWorker?.(stakePool.pid, pubkey)
-      )
+    if (action) {
+      return waitSignAndSend(action)
     }
-  }, [api, waitSignAndSend, stakePool.pid, pubkey, decimals])
+  }, [waitSignAndSend, action])
+
   const onInputChange = useCallback((value) => {
     setPubkey(value)
   }, [])
+
+  const fee = usePhalaStakePoolTransactionFee(action, polkadotAccount.address)
 
   return (
     <ActionModal
@@ -34,11 +44,15 @@ const AddWorkerModal = (props: StakePoolModalProps): JSX.Element => {
       disabled={!pubkey}>
       <Label>pid</Label>
       <Value>{stakePool.pid}</Value>
+
       <Label>Pubkey</Label>
       <Input
         placeholder="Public Key"
         value={pubkey}
         onChange={onInputChange}></Input>
+
+      <Label>Fee</Label>
+      <Value>{fee}</Value>
     </ActionModal>
   )
 }
