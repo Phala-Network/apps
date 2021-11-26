4a9dee4aae4cf9986732cc0a438f4823a85f3487
limichange <limichange@hotmail.com>
2021-11-26 18:07:48 +0800

    feat: StopModal add fee label

--- packages/app-mining/src/components/WorkerTable/StopModal.tsx
+++ packages/app-mining/src/components/WorkerTable/StopModal.tsx
@@ -1,5 +1,6 @@
-import {useApiPromise} from '@phala/react-libs'
-import {useCallback} from 'react'
+import {usePolkadotAccountAtom} from '@phala/app-store'
+import {useApiPromise, usePhalaStakePoolTransactionFee} from '@phala/react-libs'
+import {useCallback, useMemo} from 'react'
 import {WorkerModalProps} from '.'
 import useWaitSignAndSend from '../../hooks/useWaitSignAndSend'
 import ActionModal, {Label, Value} from '../ActionModal'
@@ -8,14 +9,22 @@ const StopModal = (props: WorkerModalProps): JSX.Element => {
   const {worker, onClose} = props
   const {api} = useApiPromise()
   const waitSignAndSend = useWaitSignAndSend()
+  const [polkadotAccount] = usePolkadotAccountAtom()
+
+  const action = useMemo(() => {
+    if (api) {
+      return api.tx.phalaStakePool?.stopMining?.(worker.pid, worker.pubkey)
+    } else {
+      return
+    }
+  }, [api, worker.pid, worker.pubkey])
+  const fee = usePhalaStakePoolTransactionFee(action, polkadotAccount.address)
 
   const onConfirm = useCallback(async () => {
-    if (api) {
-      return waitSignAndSend(
-        api.tx.phalaStakePool?.stopMining?.(worker.pid, worker.pubkey)
-      )
+    if (action) {
+      return waitSignAndSend(action)
     }
-  }, [api, waitSignAndSend, worker.pid, worker.pubkey])
+  }, [action, waitSignAndSend])
 
   return (
     <ActionModal
@@ -27,6 +36,8 @@ const StopModal = (props: WorkerModalProps): JSX.Element => {
       <Value>{worker.pid}</Value>
       <Label>WorkerPublicKey</Label>
       <Value>{worker.pubkey}</Value>
+      <Label>Fee</Label>
+      <Value>{fee}</Value>
     </ActionModal>
   )
 }
