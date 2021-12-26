0a9d7458220e4fbe36df74ccb7046350839f8383
limichange <limichange@hotmail.com>
2021-12-26 22:28:59 +0800

    feat: update transferPHAFromKhalaToKarura

--- packages/app-bridge-v2/src/xtransfer.ts
+++ packages/app-bridge-v2/src/xtransfer.ts
@@ -14,7 +14,26 @@ export function transferPHAFromKhalaToKarura(
   callback?.(`From ${sender.address} to ${recipient.address}`)
   callback?.(`Transfer PHA from Khala to Karura...`)
   return khalaApi?.tx?.xcmTransfer
-    ?.transferNative?.(karuraParaId, recipient.address, amount, 6000000000)
+    ?.transferNative?.(
+      khalaApi.createType('XcmV1MultiLocation', {
+        parents: 1,
+        interior: khalaApi.createType('Junctions', {
+          X2: [
+            khalaApi.createType('XcmV1Junction', {
+              Parachain: khalaApi.createType('Compact<U32>', karuraParaId),
+            }),
+            khalaApi.createType('XcmV1Junction', {
+              AccountId32: {
+                network: khalaApi.createType('XcmV0JunctionNetworkId', 'Any'),
+                id: '0x' + Buffer.from(recipient.publicKey).toString('hex'),
+              },
+            }),
+          ],
+        }),
+      }),
+      amount,
+      6000000000
+    )
     .signAndSend(sender, (result: any) => {
       callback?.(`Transfer PHA from Khala to Karura: ${result.status}`)
     })
