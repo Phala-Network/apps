666d1ae089b3d05d0c8b671272f8fc4a7725dc10
limichange <limichange@hotmail.com>
2021-12-27 14:18:12 +0800

    feat: header style

---
+++ packages/app-bridge-v2/src/TransferPanel/Title.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Title = styled.h1`
+  /* 大标题 M */
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 36px;
+
+  margin: 0;
+
+  /* Bk 001 */
+  color: #111111;
+`
--- packages/app-bridge-v2/src/TransferPanel/TransferPanel.tsx
+++ packages/app-bridge-v2/src/TransferPanel/TransferPanel.tsx
@@ -1,9 +1,15 @@
+import {Description} from './Description'
+import {Header} from './Header'
 import {NetworkSelect} from './NetworkSelect'
+import {Title} from './Title'
 
 export const TransferPanel = () => {
   return (
     <div>
-      TransferPanel
+      <Header>
+        <Title>BRIDGE</Title>
+        <Description>Transfer assets between chains</Description>
+      </Header>
       <NetworkSelect />
     </div>
   )
