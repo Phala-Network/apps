51463e8b8d53694053911b6e95c951b0f9abb1b8
limichange <limichange@hotmail.com>
2021-12-27 14:23:43 +0800

    feat: Header component

--- packages/app-bridge-v2/src/TransferPanel/TransferPanel.tsx
+++ packages/app-bridge-v2/src/TransferPanel/TransferPanel.tsx
@@ -1,15 +1,10 @@
-import {Description} from './Description'
 import {Header} from './Header'
 import {NetworkSelect} from './NetworkSelect'
-import {Title} from './Title'
 
 export const TransferPanel = () => {
   return (
     <div>
-      <Header>
-        <Title>BRIDGE</Title>
-        <Description>Transfer assets between chains</Description>
-      </Header>
+      <Header />
       <NetworkSelect />
     </div>
   )
