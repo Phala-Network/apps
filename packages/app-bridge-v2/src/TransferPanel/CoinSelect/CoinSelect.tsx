66b812c481c4562446959f67257c1c4dfcd2d0ff
limichange <limichange@hotmail.com>
2022-01-02 22:00:57 +0800

    fix: file path error

--- packages/app-bridge-v2/src/TransferPanel/CoinSelect/CoinSelect.tsx
+++ packages/app-bridge-v2/src/TransferPanel/CoinSelect/CoinSelect.tsx
@@ -1,6 +1,6 @@
 import {Select} from 'baseui/select'
 import * as React from 'react'
-import {coins} from '../config'
+import {coins} from '../../config'
 
 export const CoinSelect = () => {
   const [value, setValue] = React.useState<any>([{label: 'Phala'}])
