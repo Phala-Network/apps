079bb425af98dbdfc241beb35495fd4816660648
Leona Yang <leona.y.young@gmail.com>
2022-01-06 12:11:51 +0800

    update modal style

--- packages/app/src/theme.ts
+++ packages/app/src/theme.ts
@@ -1,5 +1,6 @@
 import {createLightTheme} from 'baseui'
 import {DefaultTheme} from 'styled-components'
+import {Breakpoints, MediaQuery} from 'baseui/theme'
 
 const theme: DefaultTheme = {
   colors: {
@@ -8,9 +9,29 @@ const theme: DefaultTheme = {
   },
 }
 
-export const baseTheme = createLightTheme({
-  // TODO: add base theme here
-  // primaryFontFamily: 'Lato',
-})
+const breakpoints: Breakpoints = {
+  small: 576,
+  medium: 768,
+  large: 992,
+}
+
+const mediaQuery: MediaQuery = {
+  small: '@media screen and (max-width: 576px)',
+  medium: '@media screen and (max-width: 768px)',
+  large: '@media screen and (max-width: 992px)',
+}
+
+const overrides = {
+  breakpoints,
+  mediaQuery,
+}
+
+export const baseTheme = createLightTheme(
+  {
+    // TODO: add base theme here
+    // primaryFontFamily: 'Lato',
+  },
+  overrides
+)
 
 export default theme
