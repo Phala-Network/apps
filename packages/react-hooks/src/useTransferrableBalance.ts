0f00be3e5a92e62804db3d87b9262331f905e777
limichange <limichange@hotmail.com>
2022-01-07 13:21:31 +0800

    feat: add useTransferrableBalance

---
+++ packages/react-hooks/src/useTransferrableBalance.ts
@@ -0,0 +1,36 @@
+import {useApiPromise} from '@phala/react-libs'
+import type {AccountId} from '@polkadot/types/interfaces'
+import BN from 'bn.js'
+import {useEffect, useState} from 'react'
+
+export default function useTransferrableBalance(
+  address?: string | AccountId | Uint8Array
+): BN | undefined {
+  const {api, readystate} = useApiPromise()
+  const [balance, setBalance] = useState<BN>()
+  const initialized = readystate === 'ready'
+
+  useEffect(() => {
+    if (!address || !initialized || !api) {
+      return
+    }
+
+    let unsubscribe: () => void
+
+    // max(0, data.free - max(data.frozenAll, data.miscFrozen, data.feeFrozen))
+    api.query.system
+      .account(address, ({data}) => {
+        const miscFrozen = new BN(data.miscFrozen)
+        const feeFrozen = new BN(data.feeFrozen)
+        const maxFrozen = BN.max(miscFrozen, feeFrozen)
+        setBalance(BN.max(new BN(0), new BN(data.free).sub(maxFrozen)))
+      })
+      .then((_unsubscribe) => (unsubscribe = _unsubscribe))
+
+    return () => {
+      unsubscribe?.()
+    }
+  }, [api, initialized, address])
+
+  return balance
+}
