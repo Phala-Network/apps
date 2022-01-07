d772db92314c785358d0843c3dc7f2e16e97df83
limichange <limichange@hotmail.com>
2022-01-07 13:31:43 +0800

    feat: add usePolkadotAccountTransferrableBalanceDecimal

---
+++ packages/react-hooks/src/usePolkadotAccountTransferrableBalanceDecimal.ts
@@ -0,0 +1,19 @@
+import {Decimal} from 'decimal.js'
+import {useMemo} from 'react'
+import useTransferrableBalance from './useTransferrableBalance'
+
+export default function usePolkadotAccountTransferrableBalanceDecimal(
+  address?: string
+): Decimal {
+  const polkadotAccountBalance = useTransferrableBalance(address)
+
+  const polkadotAccountBalanceNumber = useMemo(
+    () =>
+      polkadotAccountBalance
+        ? new Decimal(polkadotAccountBalance.toString()).div(10 ** 12)
+        : new Decimal(-1),
+    [polkadotAccountBalance]
+  )
+
+  return polkadotAccountBalanceNumber
+}
