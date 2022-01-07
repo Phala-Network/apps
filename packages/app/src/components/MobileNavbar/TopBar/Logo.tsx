5e6d0ed72910cc7cbb92386e0beed7b92cbbcd4a
Leona Yang <leona.y.young@gmail.com>
2022-01-07 14:05:43 +0800

    update logo

--- packages/app/src/components/MobileNavbar/TopBar/Logo.tsx
+++ packages/app/src/components/MobileNavbar/TopBar/Logo.tsx
@@ -4,12 +4,14 @@ import PhalaApp from '../../../icons/phala_app.svg'
 const Wrapper = styled.div`
   padding-left: 4px;
   flex: 1;
+  display: flex;
+  align-items: center;
 `
 
 const Logo: React.FC = () => {
   return (
     <Wrapper>
-      <PhalaApp width="96" height="17.75" />
+      <PhalaApp width="115" height="21" />
     </Wrapper>
   )
 }
