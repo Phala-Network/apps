0d112a6b109d46df9a4642e7c9fdbd24845c2423
limichange <limichange@hotmail.com>
2022-01-09 15:19:37 +0800

    feat: EthereumConnectWallet component

---
+++ packages/app-bridge-v2/src/components/EthereumConnectWallet/EthereumConnectWallet.tsx
@@ -0,0 +1,36 @@
+import {useEthereumAccountAtom} from '@phala/app-store'
+import {useAccountsQuery, useEthereumWeb3, useEthers} from '@phala/react-libs'
+import {Button} from 'baseui/button'
+import {FC, useEffect} from 'react'
+
+export interface EthereumConnectWalletProps {
+  hidden?: boolean
+}
+
+export const EthereumConnectWallet: FC<EthereumConnectWalletProps> = (
+  props
+) => {
+  const {hidden = false} = props
+
+  const {data: accounts = []} = useAccountsQuery()
+  const {ethereumWeb3connect} = useEthereumWeb3()
+  const {readystate: readyState} = useEthers()
+  const isReady = readyState === 'connected'
+  const [, setEthereumAccount] = useEthereumAccountAtom()
+
+  useEffect(() => {
+    const [account] = accounts
+    if (!accounts || !isReady) {
+      return
+    }
+
+    setEthereumAccount({
+      name: account,
+      address: account || '',
+    })
+  }, [accounts, isReady, setEthereumAccount])
+
+  if (hidden) return null
+
+  return <Button onClick={ethereumWeb3connect}>Connect Wallet</Button>
+}
