9b6a2ec9d2b3aefa4efa62b4dc14375a60ee4b0a
limichange <limichange@hotmail.com>
2022-01-09 15:11:15 +0800

    feat: create useBridgePage hook

---
+++ packages/app-bridge-v2/src/useBridgePage.ts
@@ -0,0 +1,52 @@
+import {useEthereumAccountAtom, usePolkadotAccountAtom} from '@phala/app-store'
+import {
+  useEthereumAccountBalanceDecimal,
+  usePolkadotAccountTransferrableBalanceDecimal,
+} from '@phala/react-hooks'
+import {Decimal} from 'decimal.js'
+
+interface useBridgePageProps {
+  blockchainType: string
+}
+
+export function useBridgePage(props: useBridgePageProps) {
+  const {blockchainType = 'ethereum'} = props
+  const [polkadotAccount] = usePolkadotAccountAtom()
+  const polkadotAccountAddress = polkadotAccount?.address
+  const [ethereumAccount] = useEthereumAccountAtom()
+  const ethereumAccountAddress = ethereumAccount?.address
+
+  const ethereumAccountBalanceDecimal = useEthereumAccountBalanceDecimal(
+    ethereumAccountAddress
+  )
+  const polkadotAccountBalanceDecimal =
+    usePolkadotAccountTransferrableBalanceDecimal(polkadotAccountAddress)
+
+  const isFromEthereum = blockchainType === 'ethereum'
+  const isFromKhala = !isFromEthereum
+  const currentAddress = isFromEthereum
+    ? ethereumAccountAddress
+    : polkadotAccountAddress
+
+  const currentBalance = isFromEthereum
+    ? ethereumAccountBalanceDecimal
+    : polkadotAccountBalanceDecimal
+
+  const maxAmountDecimal = new Decimal(currentBalance)
+
+  const isShowRecipient = isFromEthereum
+    ? !!polkadotAccountAddress
+    : !!ethereumAccountAddress
+
+  const isShowMaxButton = maxAmountDecimal.greaterThan(0) && isFromEthereum
+
+  return {
+    maxAmountDecimal,
+    isFromKhala,
+    isFromEthereum,
+    currentBalance,
+    currentAddress,
+    isShowRecipient,
+    isShowMaxButton,
+  } as const
+}
