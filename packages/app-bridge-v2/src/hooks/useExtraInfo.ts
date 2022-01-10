719a899db5f8e22405d7d53ab382b26d32a5487b
limichange <limichange@hotmail.com>
2022-01-10 09:53:27 +0800

    feat: define useExtraInfo return type

--- packages/app-bridge-v2/src/hooks/useExtraInfo.ts
+++ packages/app-bridge-v2/src/hooks/useExtraInfo.ts
@@ -1,7 +1,10 @@
 import {useAllTransferData} from '../store'
 import {useBridgeFee} from './useBridgeFee'
 
-export function useExtraInfo() {
+export function useExtraInfo(): {
+  label: string
+  value: string
+}[] {
   const transactionInfo = useAllTransferData()
 
   const bridgeFee = useBridgeFee(transactionInfo.fromNetwork)
