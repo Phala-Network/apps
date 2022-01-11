2a1153d8288550ff077e6e5c836724837ff5f1f3
limichange <limichange@hotmail.com>
2022-01-11 16:12:45 +0800

    refactor: BalanceLabel

--- packages/app-bridge-v2/src/components/TransferFromPanel/BalanceLabel/BalanceLabel.tsx
+++ packages/app-bridge-v2/src/components/TransferFromPanel/BalanceLabel/BalanceLabel.tsx
@@ -1,3 +1,5 @@
+import currency from 'currency.js'
+import Decimal from 'decimal.js'
 import {FC} from 'react'
 import styled from 'styled-components'
 import {useBridgePage} from '../../../useBridgePage'
@@ -13,8 +15,48 @@ const Root = styled.div`
   color: #8c8c8c;
 `
 
+function trim(value: string): string {
+  const last = value[value.length - 1]
+
+  if (last === '0') {
+    return trim(value.slice(0, -1))
+  } else {
+    return value
+  }
+}
+
 export const BalanceLabel: FC = () => {
   const {currentBalance} = useBridgePage()
 
-  return <Root>Balance: {currentBalance.toString()} PHA</Root>
+  const value = currentBalance
+  const precision = 4
+  const type = 'PHA'
+  const zero = new Decimal(0)
+
+  let balanceDisplay = ''
+
+  if (!value || value.lessThan(zero)) {
+    balanceDisplay = '...'
+  } else {
+    // 1,000.0000
+    balanceDisplay = currency(value.toString(), {symbol: '', precision})
+      .format()
+      .toString()
+
+    // 1,000.
+    balanceDisplay = trim(balanceDisplay)
+
+    // 1,000
+    if (balanceDisplay[balanceDisplay.length - 1] === '.') {
+      balanceDisplay = balanceDisplay.slice(0, -1)
+    }
+  }
+
+  if (value.equals(zero)) {
+    balanceDisplay = '0'
+  }
+
+  const result = `${balanceDisplay} ${type}`.trim()
+
+  return <Root>Balance: {result}</Root>
 }
