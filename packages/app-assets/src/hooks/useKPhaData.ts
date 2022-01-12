08dd95fccff8571251cb9500103d619ec792d534
Leona Yang <leona.y.young@gmail.com>
2022-01-12 16:40:13 +0800

    use transferrable value

--- packages/app-assets/src/hooks/useKPhaData.ts
+++ packages/app-assets/src/hooks/useKPhaData.ts
@@ -1,6 +1,9 @@
 import {useMemo} from 'react'
 import {usePolkadotAccountAtom} from '@phala/app-store'
-import {useBalance} from '@phala/react-hooks'
+import {
+  useBalance,
+  usePolkadotAccountTransferrableBalanceDecimal,
+} from '@phala/react-hooks'
 import {toFixed} from '@phala/utils'
 import {Decimal} from 'decimal.js'
 import usePHAPrice from './usePHAPrice'
@@ -10,6 +13,8 @@ const useKPhaData = () => {
   const [polkadotAccount] = usePolkadotAccountAtom()
   const polkadotAccountAddress = polkadotAccount?.address
   const polkadotAccountBalance = useBalance(polkadotAccountAddress)
+  const polkadotTransferBalanceDecimal =
+    usePolkadotAccountTransferrableBalanceDecimal(polkadotAccountAddress)
 
   // NOTE: copied from InputDataStep.tsx
   const polkadotAccountBalanceNumber = useMemo<Decimal | undefined>(
@@ -29,11 +34,16 @@ const useKPhaData = () => {
     return `$ ${toFixed(polkadotAccountBalanceNumber.mul(PHAPrice), 2)}`
   }, [polkadotAccountBalanceNumber, PHAPrice])
 
+  const transferrableValue = useMemo(() => {
+    if (!polkadotTransferBalanceDecimal) return '-'
+    return `${toFixed(polkadotTransferBalanceDecimal)} PHA`
+  }, [polkadotTransferBalanceDecimal])
+
   return {
     name: 'K-PHA',
     icon: '/images/Phala.svg',
     balance: balanceValue,
-    transferrable: '36666.2333 PHA',
+    transferrable: transferrableValue,
     crowdloanVesting: '36666.2333 PHA',
     delegate: '36666.2333 PHA',
     value: dollarValue,
