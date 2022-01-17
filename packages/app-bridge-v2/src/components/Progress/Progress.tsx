c071267440ef04e4d0cab5b3e9368e5e6f63f8e7
limichange <limichange@hotmail.com>
2022-01-17 10:19:44 +0800

    feat: update Progress style

--- packages/app-bridge-v2/src/components/Progress/Progress.tsx
+++ packages/app-bridge-v2/src/components/Progress/Progress.tsx
@@ -1,3 +1,4 @@
+import {NumberedStep, ProgressSteps} from 'baseui/progress-steps'
 import {FC} from 'react'
 import styled from 'styled-components'
 import {TimeIcon} from './TimeIcon'
@@ -7,26 +8,23 @@ const Root = styled.div`
 `
 
 const ProgressItem = styled.div<{isActive: boolean}>`
-  font-family: Montserrat;
-  font-size: 20px;
-  line-height: 20px;
-  font-weight: 400;
-
   color: ${({isActive}) => (isActive ? '#AAD829' : '#494949')};
-  margin: 4px;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  margin: 16px 0;
 `
 
 const Link = styled.a`
   text-decoration: underline;
   color: inherit;
+
+  &:hover {
+    color: #aad829;
+  }
 `
 
 const Icon = styled.div`
   margin-top: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 export interface ProgressProps {
@@ -43,28 +41,48 @@ export const Progress: FC<ProgressProps> = (props) => {
 
   return (
     <Root>
-      {items.map((item, index) => {
-        const isCurrent = progressIndex === index
-        const isActive = progressIndex >= index
-        const isLast = index === items.length - 1
+      <ProgressSteps current={progressIndex}>
+        {items.map((item, index) => {
+          const isCurrent = progressIndex === index
+          const isActive = progressIndex >= index
+          const isLast = index === items.length - 1
+          const titleNode = (
+            <ProgressItem isActive={isCurrent}>
+              {item.link ? (
+                <Link href={item.link} target="_blank">
+                  {item.text}
+                </Link>
+              ) : (
+                item.text
+              )}
+            </ProgressItem>
+          )
 
-        return (
-          <ProgressItem isActive={isActive} key={item.text}>
-            {item.link ? (
-              <Link href={item.link} target="_blank">
-                {item.text}
-              </Link>
-            ) : (
-              item.text
-            )}
-            {isCurrent && !isLast && (
-              <Icon>
-                <TimeIcon />
-              </Icon>
-            )}
-          </ProgressItem>
-        )
-      })}
+          return (
+            <NumberedStep
+              overrides={{
+                Root: {
+                  style: {
+                    margin: 0,
+                    padding: 0,
+                  },
+                },
+              }}
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              title={titleNode}
+              isActive={isActive}
+              key={item.text}
+            >
+              {isCurrent && !isLast && (
+                <Icon>
+                  <TimeIcon />
+                </Icon>
+              )}
+            </NumberedStep>
+          )
+        })}
+      </ProgressSteps>
     </Root>
   )
 }
