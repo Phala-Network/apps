3927750b13497965c716e26c99f867933aa5bf02
limichange <limichange@hotmail.com>
2022-01-17 12:58:40 +0800

    feat: resume Transactions

--- packages/app-bridge-v2/src/BridgePage.tsx
+++ packages/app-bridge-v2/src/BridgePage.tsx
@@ -2,11 +2,13 @@ import {Button, ToasterContainer} from '@phala/react-components'
 import {PLACEMENT} from 'baseui/toast'
 import {FC, useState} from 'react'
 import {Helmet} from 'react-helmet'
+import {QueryClient, QueryClientProvider} from 'react-query'
 import {ExchangeIcon} from './components/ExchangeIcon'
 import {ExtraInfoPanel} from './components/ExtraInfoPanel'
 import {Header} from './components/Header'
 import {InformationModal} from './components/InformationModal'
 import {SubmitButton} from './components/SubmitButton'
+import {Transactions} from './components/Transactions'
 import {TransferFromPanel} from './components/TransferFromPanel'
 import {TransferModal} from './components/TransferModal'
 import {TransferToPanel} from './components/TransferToPanel'
@@ -14,7 +16,7 @@ import {useExtraInfo} from './hooks/useExtraInfo'
 import {useAllTransferData, useSwitchToAndFormData} from './store'
 import {BlockItem, MainContent, Root} from './styledComponents'
 
-// const queryClient = new QueryClient()
+const queryClient = new QueryClient()
 
 export const BridgePage: FC = () => {
   const transactionInfo = useAllTransferData()
@@ -28,65 +30,67 @@ export const BridgePage: FC = () => {
   }
 
   return (
-    <Root>
-      <Helmet>
-        <title>Bridge</title>
-      </Helmet>
+    <QueryClientProvider contextSharing={true} client={queryClient}>
+      <Root>
+        <Helmet>
+          <title>Bridge</title>
+        </Helmet>
 
-      <MainContent>
-        <Header />
+        <MainContent>
+          <Header />
 
-        <BlockItem>
-          <TransferFromPanel />
+          <BlockItem>
+            <TransferFromPanel />
 
-          <ExchangeIcon onClick={switchData} />
+            <ExchangeIcon onClick={switchData} />
 
-          <TransferToPanel />
-        </BlockItem>
+            <TransferToPanel />
+          </BlockItem>
 
-        <BlockItem>
-          <ExtraInfoPanel infos={extraInfo} />
-        </BlockItem>
+          <BlockItem>
+            <ExtraInfoPanel infos={extraInfo} />
+          </BlockItem>
 
-        <BlockItem>
-          <SubmitButton onSubmit={submit} />
-        </BlockItem>
-      </MainContent>
+          <BlockItem>
+            <SubmitButton onSubmit={submit} />
+          </BlockItem>
+        </MainContent>
 
-      <TransferModal
-        transactionInfo={transactionInfo}
-        onClose={() => {
-          setIsOpenTransferModal(false)
-        }}
-        isOpen={isOpenTransferModal}
-      />
+        <TransferModal
+          transactionInfo={transactionInfo}
+          onClose={() => {
+            setIsOpenTransferModal(false)
+          }}
+          isOpen={isOpenTransferModal}
+        />
 
-      <InformationModal
-        onClose={() => {
-          setIsOpenInformationModal(false)
-        }}
-        isOpen={isOpenInformationModal}
-      />
+        <InformationModal
+          onClose={() => {
+            setIsOpenInformationModal(false)
+          }}
+          isOpen={isOpenInformationModal}
+        />
 
-      <Button
-        onClick={() => {
-          setIsOpenTransferModal(true)
-        }}
-      >
-        提交窗口
-      </Button>
+        <Button
+          onClick={() => {
+            setIsOpenTransferModal(true)
+          }}
+        >
+          提交窗口
+        </Button>
 
-      <Button
-        onClick={() => {
-          setIsOpenInformationModal(true)
-        }}
-      >
-        信息窗口
-      </Button>
+        <Button
+          onClick={() => {
+            setIsOpenInformationModal(true)
+          }}
+        >
+          信息窗口
+        </Button>
 
-      <ToasterContainer placement={PLACEMENT.topRight} />
+        <ToasterContainer placement={PLACEMENT.topRight} />
 
-      {/* <Transactions /> */}
-    </Root>
+        <Transactions />
+      </Root>
+    </QueryClientProvider>
   )
 }
