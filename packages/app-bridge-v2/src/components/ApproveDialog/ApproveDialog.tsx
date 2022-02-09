5be2d4151c5e6645fdc26fc80b4b8d452f61bdf3
limichange <limichange@hotmail.com>
2022-02-09 17:48:17 +0800

    feat: adjust ApproveDialog style

--- packages/app-bridge-v2/src/components/ApproveDialog/ApproveDialog.tsx
+++ packages/app-bridge-v2/src/components/ApproveDialog/ApproveDialog.tsx
@@ -21,6 +21,7 @@ import {ethers} from 'ethers'
 import React, {useEffect, useState} from 'react'
 import {buttonOverrides} from '../../style/buttonOverrides'
 import {modalOverrides} from '../../style/modalOverrides'
+import {Button} from '../Button'
 
 type Props = {
   visible: boolean
@@ -71,7 +72,7 @@ export const ApproveDialog: React.FC<Props> = (props) => {
 
   return (
     <Modal
-      closeable={true}
+      closeable={false}
       onClose={onClose}
       isOpen={visible}
       animate
@@ -85,28 +86,51 @@ export const ApproveDialog: React.FC<Props> = (props) => {
       <ModalBody>
         In order for the bridge to move your ERC20 tokens to Khala Network, it
         first needs your approval. This is only required once per ERC20 token!
+        <Block marginTop={['20px']}>
+          <FeeLabel label="Fee" fee={(fee?.toFixed(6) || '') + ' ETH'} />
+        </Block>
       </ModalBody>
 
       <ModalFooter>
         <Block
           display="flex"
           justifyContent="space-between"
-          alignItems="center"
+          alignItems="stretch"
         >
-          <FeeLabel label="Fee" fee={(fee?.toFixed(6) || '') + ' ETH'} />
-
-          <Block>
-            <ModalButton onClick={onClose} kind={ButtonKind.tertiary}>
+          <Block flex={1}>
+            <ModalButton
+              overrides={{
+                BaseButton: {
+                  style: {
+                    backgroundColor: '#EEEEEE',
+                    width: '100%',
+                  },
+                },
+              }}
+              onClick={onClose}
+              kind={ButtonKind.tertiary}
+            >
               Reject
             </ModalButton>
+          </Block>
 
-            <ModalButton
-              overrides={buttonOverrides}
+          <Block width={['20px']} />
+
+          <Block flex={1}>
+            <Button
+              overrides={{
+                BaseButton: {
+                  style: {
+                    width: '100%',
+                    ...buttonOverrides.BaseButton.style,
+                  },
+                },
+              }}
               isLoading={isSubmitting}
               onClick={startApprove}
             >
               Confirm
-            </ModalButton>
+            </Button>
           </Block>
         </Block>
       </ModalFooter>
