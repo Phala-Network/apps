302b0b6b7920949f31921a0a1fdb22013e8692e3
limichange <limichange@hotmail.com>
2022-02-11 15:53:30 +0800

    feat: add TransactionTable component

--- packages/app-bridge-transaction/src/AppBridgeTransactionPage.tsx
+++ packages/app-bridge-transaction/src/AppBridgeTransactionPage.tsx
@@ -1,11 +1,16 @@
 import {FC} from 'react'
+import {TransactionTable} from './TransactionTable'
 
 export interface AppBridgeTransactionPageProps {
   children: React.ReactNode
 }
 
-export const AppBridgeTransactionPage: FC<AppBridgeTransactionPageProps> = (
-  props
-) => {
-  return <div>{props.children}</div>
+export const AppBridgeTransactionPage: FC<
+  AppBridgeTransactionPageProps
+> = () => {
+  return (
+    <div>
+      <TransactionTable />
+    </div>
+  )
 }
