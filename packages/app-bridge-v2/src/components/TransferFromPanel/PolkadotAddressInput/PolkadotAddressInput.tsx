0edeac93b397f51cae17959c0998714378c74f79
limichange <limichange@hotmail.com>
2022-02-16 16:33:56 +0800

    feat: Polkadot select account

---
+++ packages/app-bridge-v2/src/components/TransferFromPanel/PolkadotAddressInput/PolkadotAddressInput.tsx
@@ -0,0 +1,31 @@
+import {usePolkadotAccountAtom} from '@phala/app-store'
+import {Block} from 'baseui/block'
+import {FC, useEffect} from 'react'
+import {useFromAddress} from '../../../store'
+import {PolkadotConnectWallet} from '../../PolkadotConnectWallet'
+import {Address, Root, WalletName} from './styledComponents'
+
+export interface PolkadotAddressInputProps {
+  children?: React.ReactNode
+}
+
+export const PolkadotAddressInput: FC<PolkadotAddressInputProps> = () => {
+  const [polkadotAccount] = usePolkadotAccountAtom()
+  const [, setFromAddress] = useFromAddress()
+
+  useEffect(() => {
+    if (polkadotAccount?.address) {
+      setFromAddress(polkadotAccount?.address)
+    }
+  }, [polkadotAccount?.address, setFromAddress])
+
+  return (
+    <Root>
+      <Block>
+        <WalletName>{polkadotAccount?.name}</WalletName>
+        <PolkadotConnectWallet />
+      </Block>
+      <Address>{polkadotAccount?.address}</Address>
+    </Root>
+  )
+}
