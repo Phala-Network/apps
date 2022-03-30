76ea69114f25d1c83e5f7c727f741fc0a21da482
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2022-03-30 17:00:34 +0800

    remove unused env type

--- packages/app-mining/src/global.d.ts
+++ packages/app-mining/src/global.d.ts
@@ -2,13 +2,3 @@ declare module '*.png' {
   const content: any
   export default content
 }
-
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      GATSBY_METABASE_SECRET_KEY: string
-    }
-  }
-}
-
-export {}
