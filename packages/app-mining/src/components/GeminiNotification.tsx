1f349488c9962500c268d987f5bc8f59749e7e4f
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2022-04-02 17:23:51 +0800

    get current block

--- packages/app-mining/src/components/GeminiNotification.tsx
+++ packages/app-mining/src/components/GeminiNotification.tsx
@@ -4,9 +4,23 @@ import {Info} from 'react-feather'
 import {Block} from 'baseui/block'
 import {useStyletron} from 'baseui'
 import {StyledLink} from 'baseui/link'
+import {useApiPromise} from '@phala/react-libs'
+import {useQuery} from 'react-query'
 
 const GeminiNotification: VFC = () => {
   const [css] = useStyletron()
+  const {api, initialized} = useApiPromise()
+
+  const {data} = useQuery(
+    ['geminiNotificationCurrentBlock', initialized],
+    () => api?.rpc.chain.getBlock(),
+    {
+      refetchInterval: 20000,
+    }
+  )
+
+  const blockNumber = data?.block.header.number.toJSON() as number
+
   return (
     <Notification
       overrides={{
@@ -22,8 +36,9 @@ const GeminiNotification: VFC = () => {
       <Block display="flex" alignItems="center">
         <Info size={16} className={css({marginRight: '12px', flex: 'none'})} />
         <span>
-          Gemini Tokenomics will launch at block height #1467000, it is
-          estimated to be April 8.{' '}
+          Gemini Tokenomics will launch at block height #1467000,{' '}
+          {blockNumber && `${Math.max(1467000 - blockNumber, 0)} blocks left, `}
+          it is estimated to be April 8.{' '}
           <StyledLink
             href="https://medium.com/phala-network/must-read-before-gemini-upgrade-580e0ea4b56b"
             target="_blank"
