2cc09d79548302cbadcb93ccd080189959d0f863
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2022-04-09 04:57:42 +0800

    add auto connect wallet hook and fix some warnings

--- packages/app-store/src/customEndpointAtom.ts
+++ packages/app-store/src/customEndpointAtom.ts
@@ -2,7 +2,7 @@ import {useAtom} from 'jotai'
 import {atomWithStorage} from 'jotai/utils'
 
 export const customEndpointAtom = atomWithStorage<string>(
-  'customEndpoint',
+  'jotai:custom_endpoint',
   'wss://khala-api.phala.network/ws'
 )
 
--- packages/app/src/hooks/useAutoConnectWallet.ts
+++ packages/app/src/hooks/useAutoConnectWallet.ts
@@ -1,10 +1,34 @@
-import {useLastWalletExtensionName} from '@phala/app-store'
+import {useLastWalletExtensionName, useWallet} from '@phala/app-store'
 import {useEffect} from 'react'
 
+/**
+ * Hook to automatically connect to the last wallet extension
+ */
 export const useAutoConnectWallet = () => {
+  const [wallet, setWallet] = useWallet()
   const [lastWalletExtensionName] = useLastWalletExtensionName()
 
   useEffect(() => {
-    // TODO: auto connect wallet here
-  }, [lastWalletExtensionName])
+    if (wallet || !lastWalletExtensionName) return
+    let unmounted = false
+
+    import('@phala/wallets').then(({getWalletBySource}) => {
+      if (unmounted) return
+      const wallet = getWalletBySource(lastWalletExtensionName)
+
+      if (wallet) {
+        ;(wallet.enable() as Promise<void>)
+          .then(() => {
+            if (unmounted) return
+            setWallet(wallet)
+          })
+          // Silent auto connect errors
+          .catch(() => null)
+      }
+    })
+
+    return () => {
+      unmounted = true
+    }
+  }, [lastWalletExtensionName, setWallet, wallet])
 }
