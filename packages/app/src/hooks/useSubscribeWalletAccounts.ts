7d86a7a5c79c7ce0c42b1de9bc43901ad03640dc
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2022-04-12 01:39:23 +0800

    fix accounts transformation

--- packages/app/src/hooks/useSubscribeWalletAccounts.ts
+++ packages/app/src/hooks/useSubscribeWalletAccounts.ts
@@ -1,6 +1,15 @@
 import {useAccounts, useWallet} from '@phala/app-store'
 import {useEffect} from 'react'
 import {encodeAddress} from '@polkadot/util-crypto'
+import {WalletAccount} from '@phala/wallets/types'
+
+const transformAccounts = (accounts: WalletAccount[]): WalletAccount[] => {
+  return accounts.map((account) => ({
+    ...account,
+    // FIXME: hardcode ss58Format to 30, should use api.registry.chainSS58
+    address: encodeAddress(account.address, 30),
+  }))
+}
 
 export const useSubscribeWalletAccounts = () => {
   const [wallet] = useWallet()
@@ -11,18 +20,12 @@ export const useSubscribeWalletAccounts = () => {
     if (wallet) {
       // Some wallets don't implement subscribeAccounts correctly, so call getAccounts anyway
       wallet.getAccounts().then((accounts) => {
-        setAccounts(accounts)
+        setAccounts(accounts ? transformAccounts(accounts) : null)
 
         wallet
           .subscribeAccounts((accounts) => {
             if (accounts) {
-              setAccounts(
-                accounts.map((account) => ({
-                  ...account,
-                  // FIXME: hardcode ss58Format to 30, should use api.registry.chainSS58
-                  address: encodeAddress(account.address, 30),
-                }))
-              )
+              setAccounts(transformAccounts(accounts))
             }
           })
           .then((_unsub) => {
