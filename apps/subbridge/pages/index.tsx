b798164722ffbbd07276891c04fc480a3740c05d
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2022-04-13 20:22:46 +0800

    feat(subbridge): add app

---
+++ apps/subbridge/pages/index.tsx
@@ -0,0 +1,16 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>SubBridge</title>
+        <meta name="description" content="SubBridge" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+    </div>
+  )
+}
+
+export default Home
