375c17b7eb0d7db90eaf6b01ab6b3d73f3a4e1cf
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2022-04-19 02:42:01 +0800

    add emotion

--- apps/subbridge/pages/_app.tsx
+++ apps/subbridge/pages/_app.tsx
@@ -1,7 +1,30 @@
-import type {AppProps} from 'next/app'
+import Head from 'next/head'
+import {AppProps} from 'next/app'
+import {ThemeProvider} from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import {CacheProvider, EmotionCache} from '@emotion/react'
+import {theme} from '../src/theme'
+import createEmotionCache from '../src/createEmotionCache'
 
-function MyApp({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
 }
 
-export default MyApp
+export default function MyApp(props: MyAppProps) {
+  const {Component, emotionCache = clientSideEmotionCache, pageProps} = props
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
+}
