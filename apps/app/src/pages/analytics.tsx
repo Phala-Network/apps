3dbc45c8643c1dcbe68f1bc3cc287a60b74eb26a
Kingsley <10992364+kingsleydon@users.noreply.github.com>
2022-04-26 15:31:54 +0800

    fix(analytics): iframe auto resize (#137)

--- apps/app/src/pages/analytics.tsx
+++ apps/app/src/pages/analytics.tsx
@@ -1,21 +1,22 @@
 import {useStyletron} from 'baseui'
 import {Block} from 'baseui/block'
+import IframeResizer from 'iframe-resizer-react'
 import {FC} from 'react'
 import {Helmet} from 'react-helmet'
 
 const Analytics: FC = () => {
   const [css] = useStyletron()
+
   return (
     <Block marginTop="scale600" marginBottom="scale600">
       <Helmet title="Analytics"></Helmet>
-      <iframe
+      <IframeResizer
         className={css({
           width: 'calc(100% + 32px)',
-          height: '1000px',
           border: 'none',
         })}
         src="https://mb.phala.network/public/dashboard/d3e95e99-dcd9-4469-ad32-7e5dbd388cc7#bordered=false"
-      ></iframe>
+      ></IframeResizer>
     </Block>
   )
 }
