faafe67f0b82156c595fb40f6a36e6350eee65ba
Kingsley <10992364+kingsleydon@users.noreply.github.com>
2022-06-13 16:47:39 +0800

    feat(app): use real phala endpoint (#177)

--- apps/app/src/hooks/useTestnetGuard.ts
+++ apps/app/src/hooks/useTestnetGuard.ts
@@ -1,13 +1,13 @@
+import {useLocation} from '@reach/router'
 import {useEffect} from 'react'
 import {useCurrentNetworkNode} from '../store/networkNode'
-import {useLocation} from '@reach/router'
 
 export const useTestnetGuard = () => {
   const {pathname} = useLocation()
   const [currentNetworkNode] = useCurrentNetworkNode()
 
   useEffect(() => {
-    if (pathname !== '/' && currentNetworkNode.id === 'phala-rewards-demo') {
+    if (pathname !== '/' && currentNetworkNode.id === 'phala-via-phala') {
       location.href = '/'
     }
   }, [currentNetworkNode.id, pathname])
