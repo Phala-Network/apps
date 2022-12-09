d9dcb0dcc8a37355f42cce5d7ea9e7d92e91d8b8
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2022-12-10 01:35:41 +0800

    change endpoint to pc4

--- apps/app/config.ts
+++ apps/app/config.ts
@@ -1,4 +1,4 @@
-export const KHALA_ENDPOINTS = ['wss://pc-test-3.phala.network/khala/ws']
+export const KHALA_ENDPOINTS = ['wss://pc-test-4.phala.network/khala/ws']
 // export const KHALA_ENDPOINTS = [
 //   'wss://khala-api.phala.network/ws',
 //   'wss://khala.api.onfinality.io/public-ws',
