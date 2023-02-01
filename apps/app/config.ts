e1384fe58d48a09d7ce844ccd6aa0883cdc31acb
Kingsley <10992364+kingsleydon@users.noreply.github.com>
2023-02-01 16:47:01 +0800

    chore: remove pinknode rpc (#261)

--- apps/app/config.ts
+++ apps/app/config.ts
@@ -2,7 +2,6 @@ export const KHALA_ENDPOINTS = [
   'wss://khala-api.phala.network/ws',
   'wss://khala.api.onfinality.io/public-ws',
   'wss://khala-rpc.dwellir.com',
-  'wss://public-rpc.pinknode.io/khala',
 ]
 export const PHALA_ENDPOINTS = ['wss://api.phala.network/ws']
 export const WPHA_ASSET_ID = 10000
