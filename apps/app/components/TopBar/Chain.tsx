1c465eca0c188d09dd2657c81106a9f33eddff42
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2023-03-11 01:38:03 +0800

    feat: add block time

--- apps/app/components/TopBar/Chain.tsx
+++ apps/app/components/TopBar/Chain.tsx
@@ -4,49 +4,51 @@ import {useGlobalStateQuery} from '@/lib/subsquidQuery'
 import {colors, montserrat} from '@/lib/theme'
 import {chainAtom, type Chain} from '@/store/common'
 import CircleIcon from '@mui/icons-material/Circle'
-import {MenuItem, Select, Stack, Tooltip, Typography} from '@mui/material'
-import {type VoidFn} from '@polkadot/api/types'
+import {MenuItem, Select, Stack, Tooltip} from '@mui/material'
+import {toCurrency} from '@phala/util'
 import {useAtom} from 'jotai'
-import {useEffect, useMemo, useState, type FC} from 'react'
+import {useMemo, type FC} from 'react'
+import useSWRSubscription from 'swr/subscription'
+import Property from '../Property'
 
 const ChainSelect: FC = () => {
   const api = usePolkadotApi()
   const [chain, setChain] = useAtom(chainAtom)
   const isKhala = chain === 'khala'
-  const [currentHeight, setCurrentHeight] = useState<number>()
 
   const color = `${isKhala ? '#03ffff' : colors.main[400]} !important`
   const backgroundColor = isKhala ? '#1E565E' : colors.main[700]
 
   const {data: globalStateData} = useGlobalStateQuery(subsquidClient, {})
   const subsquidHeight = globalStateData?.squidStatus?.height
+  const blockTime = globalStateData?.globalStateById?.averageBlockTime
   const apiConnected = api?.isConnected
+  const {data: chainHeight} = useSWRSubscription(
+    api != null ? ['height', api] : null,
+    (_, {next}) => {
+      let unsubscribe = (): void => {}
+      if (api == null) {
+        return unsubscribe
+      }
+      void api.rpc.chain
+        .subscribeNewHeads((header) => {
+          next(null, header.number.toNumber())
+        })
+        .then((_unsubscribe) => {
+          unsubscribe = _unsubscribe
+        })
 
-  useEffect(() => {
-    if (api == null) {
-      return
+      return () => {
+        unsubscribe()
+      }
     }
-    let unsubscribe: VoidFn
-
-    void api.rpc.chain
-      .subscribeNewHeads((header) => {
-        setCurrentHeight(header.number.toNumber())
-      })
-      .then((_unsubscribe) => {
-        unsubscribe = _unsubscribe
-      })
-
-    return () => {
-      unsubscribe?.()
-    }
-  }, [api])
-
+  )
   const status: 'success' | 'warning' | 'error' = useMemo(() => {
     if (apiConnected === true) {
       if (
         subsquidHeight != null &&
-        currentHeight != null &&
-        currentHeight - subsquidHeight < 10
+        chainHeight != null &&
+        chainHeight - subsquidHeight < 10
       ) {
         return 'success'
       }
@@ -54,7 +56,7 @@ const ChainSelect: FC = () => {
     }
 
     return 'error'
-  }, [apiConnected, subsquidHeight, currentHeight])
+  }, [apiConnected, chainHeight, subsquidHeight])
 
   return (
     <Select
@@ -90,20 +92,28 @@ const ChainSelect: FC = () => {
             {value === 'khala' && chain === 'khala' && (
               <Tooltip
                 PopperProps={{
-                  sx: {fontSize: '1rem!important'},
+                  sx: {minWidth: 200},
                   onClick: (e) => {
                     e.stopPropagation()
                   },
                 }}
                 title={
-                  <Typography variant="body2" fontWeight="500" component="div">
-                    API status:{' '}
-                    {apiConnected === true ? 'Connected' : 'Disconnected'}
-                    <br />
-                    Indexer height: {subsquidHeight ?? '-'}
-                    <br />
-                    Blockchain height: {currentHeight ?? '-'}
-                  </Typography>
+                  <>
+                    <Property label="API status" size="small" fullWidth>
+                      {apiConnected === true ? 'Connected' : 'Disconnected'}
+                    </Property>
+                    <Property label="Backend height" size="small" fullWidth>
+                      {subsquidHeight == null
+                        ? '-'
+                        : toCurrency(subsquidHeight)}
+                    </Property>
+                    <Property label="Chain height" size="small" fullWidth>
+                      {chainHeight == null ? '-' : toCurrency(chainHeight)}
+                    </Property>
+                    <Property label="Block time" size="small" fullWidth>
+                      {blockTime == null ? '-' : `${blockTime / 1000}s`}
+                    </Property>
+                  </>
                 }
               >
                 <CircleIcon color={status} sx={{width: 10}} />
