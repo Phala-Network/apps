99fc642e8db26786443cbcce960a2e967a956591
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2023-07-18 16:22:22 +0800

    feat(subbridge): phala to acala bridge

--- apps/subbridge/lib/transferByPhalaXTransfer.ts
+++ apps/subbridge/lib/transferByPhalaXTransfer.ts
@@ -8,6 +8,7 @@ import {decodeAddress} from '@polkadot/util-crypto'
 import getGeneralKey, {type Hex} from './getGeneralKey'
 
 const moonriverParaId = CHAINS.moonriver.paraId
+const acalaParaId = CHAINS.acala.paraId
 const karuraParaId = CHAINS.karura.paraId
 const bifrostKusamaParaId = CHAINS['bifrost-kusama'].paraId
 const parallelParaId = CHAINS.parallel.paraId
@@ -40,6 +41,12 @@ const assetConcrete: {
       },
     },
     astr: {parents: 1, interior: {X1: {Parachain: astarParaId}}},
+    aca: {
+      parents: 1,
+      interior: {
+        X2: [{Parachain: acalaParaId, GeneralKey: getGeneralKey('0x0003')}],
+      },
+    },
   },
   khala: {
     pha: {parents: 0, interior: 'Here'},
