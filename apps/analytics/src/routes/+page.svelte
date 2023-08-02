4f9365d83c8316caf50d3f8e683faba39557a906
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2023-08-02 17:25:22 +0800

    feat(analytics): create project

---
+++ apps/analytics/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<svelte:head>
+  <title>Phala Analytics</title>
+</svelte:head>
+
+<main class="flex flex-col items-center">
+  <header class="flex items-center my-6">
+    <img class="block w-10 mr-4" src="favicon.png" alt="Phala logo" />
+    <h1 class="text-2xl font-medium">Phala Analytics</h1>
+  </header>
+
+  <h2>Phat Contract execution count</h2>
+</main>
