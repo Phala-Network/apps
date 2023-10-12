18f8d1253532e25d08572948f65a9eab44fedd82
Kingsley <10992364+kingsleydon@users.noreply.github.com>
2023-10-13 00:33:24 +0800

    rename pathname (#346)

--- apps/index/pages/gpt.tsx
+++ apps/index/pages/gpt.tsx
@@ -1,9 +1,10 @@
-import Intent from '@/components/Intent'
+import GPT from '@/components/GPT'
 import {clientAtom} from '@/store/core'
 import {CircularProgress, Container, Stack} from '@mui/material'
 import {useAtom} from 'jotai'
 import type {NextPage} from 'next'
 import dynamic from 'next/dynamic'
+import Head from 'next/head'
 
 const PolkadotWalletDialog = dynamic(
   async () => await import('@/components/PolkadotWalletDialog'),
@@ -14,12 +15,15 @@ const Home: NextPage = () => {
   const [client] = useAtom(clientAtom)
   return (
     <Container sx={{pt: {sm: 9, md: 12}}}>
+      <Head>
+        <title>inDEX GPT</title>
+      </Head>
       {client == null ? (
         <Stack alignItems="center">
           <CircularProgress />
         </Stack>
       ) : (
-        <Intent />
+        <GPT />
       )}
 
       <PolkadotWalletDialog />
