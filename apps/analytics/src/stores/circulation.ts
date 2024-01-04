6f9c9b83f8d163c251cc047173898d106b9aee84
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2024-01-04 12:28:01 +0900

    Refactor circulation data fetching and calculation

--- apps/analytics/src/stores/circulation.ts
+++ apps/analytics/src/stores/circulation.ts
@@ -1,18 +1,13 @@
 import {createQuery} from '@tanstack/svelte-query'
 import Decimal from 'decimal.js'
 
-interface CirculationData {
-  data?: {circulations?: {nodes?: [{amount: string}?]}}
-}
-
 const fetchCirculationData = async () => {
   const res = await fetch(
-    'https://api.subquery.network/sq/Phala-Network/khala-chainbridge__UGhhb?query=%7Bcirculations(first:1,orderBy:BLOCK_HEIGHT_DESC)%7Bnodes%7Bamount%7D%7D%7D',
+    'https://pha-circulation-server.vercel.app/api/circulation',
   )
-  const data = (await res.json()) as CirculationData
-  const amount = data.data?.circulations?.nodes?.[0]?.amount
+  const amount = await res.text()
   if (amount != null) {
-    return new Decimal(amount).div(1e12)
+    return new Decimal(amount)
   }
 }
 
