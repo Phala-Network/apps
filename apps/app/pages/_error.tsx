bf6500777c207c1a7258e05421a0d6a93cc94d29
Kingsley <10992364+kingsleydon@users.noreply.github.com>
2024-01-22 16:13:22 +0800

    feat(app): sentry integration (#390)

---
+++ apps/app/pages/_error.tsx
@@ -0,0 +1,18 @@
+import * as Sentry from '@sentry/nextjs'
+import {type NextPage} from 'next'
+import Error, {type ErrorProps} from 'next/error'
+
+const CustomErrorComponent: NextPage<ErrorProps> = (props) => {
+  return <Error statusCode={props.statusCode} />
+}
+
+CustomErrorComponent.getInitialProps = async (contextData) => {
+  // In case this is running in a serverless function, await this in order to give Sentry
+  // time to send the error before the lambda exits
+  await Sentry.captureUnderscoreErrorException(contextData)
+
+  // This will contain the status code of the response
+  return await Error.getInitialProps(contextData)
+}
+
+export default CustomErrorComponent
