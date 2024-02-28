ad6d69e1a4f00971f9b1c62fb7fcc1f6400d0fa9
Kingsley <10992364+kingsleydon@users.noreply.github.com>
2024-02-28 19:36:35 +0800

    Update GraphQL endpoints (#398)

--- apps/app/config.ts
+++ apps/app/config.ts
@@ -6,7 +6,7 @@ export const KHALA_ENDPOINTS = [
   'wss://khala.api.onfinality.io/public-ws',
 ]
 export const KHALA_SUBSQUID_URL =
-  'https://subsquid.phala.network/khala-computation-next/graphql'
+  'https://subsquid.phala.network/khala-computation/graphql'
 export const khalaSubsquidClient = new GraphQLClient(KHALA_SUBSQUID_URL)
 export const PHALA_ENDPOINTS = [
   'wss://phala-rpc.dwellir.com',
@@ -14,6 +14,6 @@ export const PHALA_ENDPOINTS = [
   'wss://phala.api.onfinality.io/public-ws',
 ]
 export const PHALA_SUBSQUID_URL =
-  'https://subsquid.phala.network/phala-computation-next/graphql'
+  'https://subsquid.phala.network/phala-computation/graphql'
 export const phalaSubsquidClient = new GraphQLClient(PHALA_SUBSQUID_URL)
 export const WPHA_ASSET_ID = 10000
