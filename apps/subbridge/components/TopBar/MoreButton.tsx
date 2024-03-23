c340df333eb2fd786d69461eaabf2cfacc64618c
Kingsley <10992364+kingsleydon@users.noreply.github.com>
2024-03-23 18:57:47 +0800

    fix(subbridge): change format distance (#407)

--- apps/subbridge/components/TopBar/MoreButton.tsx
+++ apps/subbridge/components/TopBar/MoreButton.tsx
@@ -2,16 +2,20 @@ import {
   colorSchemeSettingAtom,
   moreButtonBadgeVersionAtom,
 } from '@/store/common'
-import Link from '@mui/icons-material/Link'
+import {OpenInNew} from '@mui/icons-material'
 import MoreHoriz from '@mui/icons-material/MoreHoriz'
 import {Badge, Box, Button, ButtonGroup, Menu, MenuItem} from '@mui/material'
 import {useAtom} from 'jotai'
 import {type FC, type MouseEvent, useState} from 'react'
 import {ClientOnly} from '../ClientOnly'
 
-const CURRENT_BADGE_VERSION = 1
+const CURRENT_BADGE_VERSION = 2
 
 const externalLinks = [
+  [
+    'Explorer',
+    'https://analytics.phala.network/public-dashboards/346284964a334ee3a5cc72e1523eccea?orgId=1',
+  ],
   ['Wiki', 'https://wiki.phala.network/en-us/general/subbridge/intro/'],
   ['Discord', 'https://discord.com/invite/phala'],
   ['Forum', 'https://forum.phala.network/c/mai/73-category/73'],
@@ -83,10 +87,15 @@ const MoreButton: FC = () => {
             }}
           >
             <Box
-              sx={{display: 'flex', justifyContent: 'space-between', width: 1}}
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                width: 1,
+                alignItems: 'center',
+              }}
             >
               <span>{text}</span>
-              <Link color="action" />
+              <OpenInNew color="action" fontSize="inherit" />
             </Box>
           </MenuItem>
         ))}
